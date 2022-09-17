d5793d58a8696cfbf53af2245e76fd3227fcc963
Hyunsoo <daniel.l@kakao.com>
2022-09-17 23:03:31 +0900

    파이썬 패캠 소스코드

---
+++ Basic/Chapter11/02.에러발생시키기.py
@@ -0,0 +1,9 @@
+# raise 구문을 사용해서 에러를 강제로 발생
+
+num = int(input("음수를 입력해 주세요.>>>"))
+
+try:
+    if num >= 0 :
+        raise Exception("양수는 입력 불가")
+except Exception as e:
+        print("에러 발생!", e)
