9f72771bc6426593e00d8f835e685e7e0e723e28
hyunsoolee <daniel.l@kakao.com>
2022-11-15 10:55:09 +0900

    Day_017 review

--- 100-Days-of-Python/Day_017/Day_017_review/main.py
+++ 100-Days-of-Python/Day_017/Day_017_review/main.py
@@ -1,10 +1,16 @@
 from data import question_data
 from question_model import Question
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 for i in question_data:
     a = Question(i["question"], i["correct_answer"])
     question_bank.append(a)
 
-print(question_bank[0].answer)
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+
+print(f"You've completed the quiz")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
