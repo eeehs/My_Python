407d4b063b0ed437dc060d25e7a478d0a60bf3cb
hyunsoolee <daniel.l@kakao.com>
2022-12-07 17:45:16 +0900

    Django view,url,model set

--- Fastcampus_python_master/Part_007/config/urls.py
+++ Fastcampus_python_master/Part_007/config/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shortener.views import get_user,index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("",index, name="index"),
+    path("get_user/<int:user_id>", get_user),
 ]
---
+++ Fastcampus_python_master/Part_007/shortener/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import User as U
+
+# Create your models here.
+
+class PayPlan(models.Model):
+    name = models.CharField(max_length=20)
+    price = models.IntegerField()
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class Users(AbstractUser):
+    pay_plan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
+
+class UserDetail(models.Model):
+    user = models.OneToOneField(Users, on_delete=models.CASCADE)
+    payplan = models.ForeignKey(PayPlan, on_delete=models.DO_NOTHING)
